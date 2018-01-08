ee46fd5c654a18b817717c9d017aaa8415069f4e
francois-roseberry <f_roseberry@live.fr>
2018-01-07 19:58:07 -0500

    Use the real server in dev instead of a fake one

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+const PORT = 3001;
 
 app.use(express.static('client/build'));
 
-const PORT = 3000;
+io.on('connection', () => {
+  // Send random temperature data at regular intervals
+  let seconds = 1;
+  setInterval(() => {
+    io.emit('data', { x: seconds, y: Math.random() * 10 + 20 });
+    seconds++;
+  }, 1000);
+});
 
 http.listen(PORT, () => {
 	console.log('Hydropi server listening on port ' + PORT);
