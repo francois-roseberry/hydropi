6c8386ebef909ae8b0710f0f2730a7d3a3c4d381
francois-roseberry <f_roseberry@live.fr>
2018-01-28 10:57:43 -0500

    Indicate the os the server is running on

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
+const process = require('process');
+
 const PORT = require('./src/config').PORT;
 const server = require('./src/server');
 
 server.listen(PORT, () => {
-	console.log('Hydropi server listening on port ' + PORT);
+	console.log('Hydropi server listening on port', PORT, 'on os', process.platform);
 });
