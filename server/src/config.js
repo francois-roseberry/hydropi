b3976baed3c37cb7de2fbde6051e538c6bf8c52c
francois-roseberry <f_roseberry@live.fr>
2018-02-01 20:20:22 -0500

    Update resin env check

--- server/src/config.js
+++ server/src/config.js
@@ -2,7 +2,7 @@ const process = require('process');
 
 exports.PORT = 3001;
 
-exports.ON_DEVICE = process.env.RESIN === 1;
+exports.ON_DEVICE = process.env.RESIN === "1";
 
 exports.LIGHTING_PIN = 12;
 exports.VENTILATION_PIN = 16;
