316ed45b0f4e717891968bd32718586dd414c9a5
francois-roseberry <f_roseberry@live.fr>
2019-11-24 12:44:42 -0500

    Remove the "powered_by_pi" logo dfrom the code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,6 @@ import { IntlProvider, FormattedMessage } from 'react-intl';
 import { DEFAULT_LOCALE } from '../config/Env';
 import { translations } from '../i18n/translations';
 import logo from './logo.png';
-import powered from './powered_by_pi.png';
 import Dashboard from './dashboard/Dashboard';
 import './App.css';
 
@@ -15,12 +14,12 @@ export default class App extends Component {
         <div className="app">
           <header className="app-header">
             <img src={ logo } className="app-logo" alt="logo" />
-            <h1 className="app-title"><FormattedMessage className="lol" id="app.title" /></h1>
+            <h1 className="app-title"><FormattedMessage id="app.title" /></h1>
           </header>
           <div className="content">
             <Dashboard />
           </div>
-          <img src={ powered } className="powered-logo" alt="Powered by Raspberry Pi" />
+          <img src="https://www.raspberrypi.org/app/uploads/2017/06/Powered-by-Raspberry-Pi-Logo_Outline-Colour-Screen-500x153.png" className="powered-logo" alt="Powered by Raspberry Pi" />
         </div>
       </IntlProvider>
     );
